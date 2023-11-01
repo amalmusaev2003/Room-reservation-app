33037be860168c253b54e1a4c3134ec43c686da3
Night_frontender <armusaev@stud.kantiana.ru>
2023-11-01 07:56:47 +0200

    Meeting Room
    Added the meeting room model, new endpoints and pydentic shemas to control input/output data.

---
+++ app/schemas/meeting_room.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str]
+
+class MeetingRoomCreate(BaseModel):
+    name: str = Field(..., min_length=1, max_length=100)
+
+class MeetingRoomDB(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
